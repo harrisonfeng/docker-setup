4424e878880136f649a2d1c2c943ab51f83a626e
Harrison Feng <feng.harrison@gmail.com>
2016-07-22 09:32:03 -0700

    add support for ubuntu os
    current script support centos only
    this commit is intent to support ubuntu
    
    Signed-off-by: Harrison Feng <feng.harrison@gmail.com>

--- docker_setup.sh
+++ docker_setup.sh
@@ -24,6 +24,9 @@
 # @file docker_setup.sh
 
 
+# ====== Shell script starting ======
+
+
 # Color Definition
 TXTRESET='\e[0m'
 TXTRED='\e[0;31m'
@@ -35,50 +38,93 @@ BTXTRED='\e[1;31m'
 BTXTGREEN='\e[1;32m'
 BTXTBLUE='\e[1;34m'
 
-ERROR_CODE=2 # Invalid parameters
 
-VERSION=$1
+DOCKER_VERSION=$1
+ERROR_CODE=2       # Invalid parameters
+UNSUPPORTED_OS=4   # Unsupported OS
 
 
-command_exists() { command -v "$@" > /dev/null 2>&1; }
+command_exists() {
+    command -v "$@" > /dev/null 2>&1;
+}
 
-usage(){
-    if [ -z "$VERSION" ]; then
+
+usage() {
+    if [ -z "$DOCKER_VERSION" ]; then
         echo -e "$TXTRED
-    ##############################################
+    ==============================================
     Please give me the docker version (e.g. 1.9.1)
 
-    Usage: $0 1.9.1
+    Usage:    ./`basename $0` DOCKER_VERSION
+    Example:  ./`basename $0` 1.11.2
 
     Here are versions available:
     1.7.0 1.7.1 
     1.8.0 1.8.1 1.8.2 1.8.3
     1.9.0 1.9.1 
     1.10.0 1.10.1 1.10.2 1.10.3
-    1.11.0 1.11.1
-    ##############################################
+    1.11.0 1.11.1 1.11.2
+    ==============================================
     $TXTRESET"
     exit $ERROR_CODE
     fi
 }
 
-prepare_repo() {
+
+setup_repo_ubuntu() {
+    CODENAME=$(lsb_release -c | tr -d [:blank:] | cut -d: -f2)
+
+    sudo apt-key adv --keyserver \
+    hkp://p80.pool.sks-keyservers.net:80 \
+    --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+
+    sudo touch /etc/apt/sources.list.d/docker.list
+    echo "deb https://apt.dockerproject.org/repo ubuntu-$CODENAME main" | sudo tee -a \
+    /etc/apt/sources.list.d/docker.list
+
+    sudo apt-get update
+}
+
+
+setup_repo_centos() {
     if [ ! -f /etc/yum.repos.d/docker.repo ]; then
         \cp -f docker.repo /etc/yum.repos.d/
     fi
 }
 
+
 install_docker() {
-    yum install -y docker-engine-$VERSION
-    \cp -f ./nsdocker /usr/local/bin
+    if command_exists yum; then
+        setup_repo_centos
+        yum install -y docker-engine-$DOCKER_VERSION
+    elif command_exists apt-get; then
+        setup_repo_ubuntu
+        sudo apt-get -y install docker-engine=${DOCKER_VERSION}\*
+    else
+        echo "Your OS system is not supported."
+        exit $UNSUPPORTED_OS
+    fi
 }
 
-usage
-prepare_repo
-install_docker
 
-if command_exists docker; then
-    service docker start
-else
-    echo "Docker doesn't exist, please check if it's installed correctly!"
-fi
+install_nsdocker() {
+    sudo \cp -f ./nsdocker /usr/local/bin
+}
+
+
+main() {
+    usage
+    install_docker
+    install_nsdocker
+    if command_exists docker; then
+        service docker start
+    else
+        echo "${BTXTRED}Docker doesn't exist, please check if it's installed correctly!${TXTRESET}"
+    fi
+}
+
+
+main
+
+
+# ====== Shell script ending ======
